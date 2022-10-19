2ae92741b61e332413356f48bcedfd1bda385abd
Tenerome <763263505@qq.com>
2022-10-19 22:18:36 +0800

    解决ffmeg不能覆盖导致终止问题 完成保存数据函数

--- Func.py
+++ Func.py
@@ -3,7 +3,6 @@ from os import system
 
 def median(fitness:list):
     '''求中位数'''
-    
     lf=deepcopy(fitness)
     lf.sort()
     half=len(lf)//2
@@ -12,21 +11,10 @@ def median(fitness:list):
 
 def picsTogif():
     '''将保存的图片转成视频'''
-    system('cd ./pics/ && ffmpeg -r 5 -i %d.png -vf palettegen palette.png && ffmpeg -r 5 -i %d.png -i palette.png -lavfi paletteuse output.gif')
+    system('cd ./pics/ && ffmpeg -r 5 -i %d.png -vf palettegen palette.png && ffmpeg -y -r 5 -i %d.png -i palette.png -lavfi paletteuse output.gif')
     system('cd ./pics/ && del *.png palette.png')
-    print("转换完成")
-
-def readFile(filepath:str='./data/gen.ini'):
-    '''读文件'''
-    with open(filepath,'r') as f:
-        con=f.read().rsplit(',')
-        gen=[]
-        for i in con:
-            gen.append(int(i))
-        return gen
 
 def writeFile(context:list,filepath:str='./data/gen.ini'):
-    '''写文件'''
-    context=str(context).strip('[').strip(']')
+    '''写文件:保存数据'''
     with open(filepath,'w') as f:
-        f.write(context)
+        f.write(str(context))
